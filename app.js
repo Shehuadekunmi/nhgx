ae89c44a031506106e02b3580525df11ae1dfde9
Shehuadekunmi <sirkumyemo@gmail.com>
2023-09-10 18:11:16 +0100

    update

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
 
+const currentUTCTime = document.getElementById("tz");
 
+const generateUTCTime = () => {
+  const date = new Date();
+  const utcTimeMilliseconds = date.getTime();
+  currentUTCTime.textContent = utcTimeMilliseconds;
+};
 
-  var date = new Date(); 
-  var now_utc =  Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(),
-  date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds());
-  var dt = new Date(now_utc);
-  let utcDate = new Date(dt.toLocaleString('en-NG', { timeZone: "UTC" }));
-  let tzDate = new Date(dt.toLocaleString('en-NG', { timeZone: "Africa/Lagos" }));
-  let offset1 = utcDate.getTime() - tzDate.getTime();
-  dt.setTime(dt.getTime() - offset1);
-  console.log(dt.toISOString());
- document.getElementById('tz').innerHTML = dt.toISOString().slice(0, 19)+'Z'
-
-
+generateUTCTime();
+setInterval(generateUTCTime, 1000);
 
 
 let weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][new Date().getDay()]
